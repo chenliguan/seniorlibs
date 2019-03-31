176e367a045cbf63c7e891f480fc362bc55e9d31
e <1130314814@qq.com>
2019-04-01 00:41:09 +0800

    测试View的事件体系

---
+++ event/src/main/java/com/g/event/DemoActivity_1.java
@@ -0,0 +1,70 @@
+package com.g.event;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.g.event.ui.HorizontalScrollViewEx;
+import com.g.event.utils.MyUtils;
+
+import java.util.ArrayList;
+
+/**
+ * 滑动冲突场景1-外部拦截
+ */
+public class DemoActivity_1 extends Activity {
+    private static final String TAG = "DemoActivity_1";
+
+    private HorizontalScrollViewEx mListContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_1);
+        Log.d(TAG, "onCreate");
+        initView();
+    }
+
+    private void initView() {
+        LayoutInflater inflater = getLayoutInflater();
+        mListContainer = findViewById(R.id.container);
+        final int screenWidth = MyUtils.getScreenMetrics(this).widthPixels;
+        final int screenHeight = MyUtils.getScreenMetrics(this).heightPixels;
+        for (int i = 0; i < 3; i++) {
+            ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.content_layout, mListContainer, false);
+            layout.getLayoutParams().width = screenWidth;
+            TextView textView = layout.findViewById(R.id.title);
+            textView.setText("page " + (i + 1));
+            layout.setBackgroundColor(Color.rgb(255 / (i + 1), 255 / (i + 1), 0));
+            createList(layout);
+            mListContainer.addView(layout);
+        }
+    }
+
+    private void createList(ViewGroup layout) {
+        ListView listView = layout.findViewById(R.id.list);
+        ArrayList<String> datas = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            datas.add("name " + i);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.content_list_item, R.id.name, datas);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(DemoActivity_1.this, "click item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ event/src/main/java/com/g/event/ui/ListViewEx.java
@@ -0,0 +1,79 @@
+package com.g.event.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+/**
+ * 滑动冲突场景2-内部拦截
+ * 子View
+ */
+public class ListViewEx extends ListView {
+    private static final String TAG = "ListViewEx";
+
+    private HorizontalScrollViewEx2 mHorizontalScrollViewEx2;
+
+    // 分别记录上次滑动的坐标
+    private int mLastX = 0;
+    private int mLastY = 0;
+
+    public ListViewEx(Context context) {
+        super(context);
+    }
+
+    public ListViewEx(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListViewEx(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setHorizontalScrollViewEx2(HorizontalScrollViewEx2 horizontalScrollViewEx2) {
+        mHorizontalScrollViewEx2 = horizontalScrollViewEx2;
+    }
+
+    /**
+     * 核心方法：在dispatchTouchEvent方法中，
+     * 修改ListView的dispatchTouchEvent方法中的父容器的拦截逻辑，同时让父拦截MOVE和Up事件即可
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN: {
+            mHorizontalScrollViewEx2.requestDisallowInterceptTouchEvent(true);
+            break;
+        }
+        case MotionEvent.ACTION_MOVE: {
+            int deltaX = x - mLastX;
+            int deltaY = y - mLastY;
+            Log.d(TAG, "dx:" + deltaX + " dy:" + deltaY);
+            // 在滑动过程中
+            // 当水平方向的距离大就判断水平滑动，为了能够水平滑动所以让父容器拦截事件;
+            // 当竖直方距离大于就不让父容器拦截，于是事件传递给了ListView，所以ListView能上下滑动，这就解决了冲突了
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                mHorizontalScrollViewEx2.requestDisallowInterceptTouchEvent(false); // 让父容器拦截
+            }
+            break;
+        }
+        case MotionEvent.ACTION_UP: {
+            break;
+        }
+        default:
+            break;
+        }
+
+        mLastX = x;
+        mLastY = y;
+        return super.dispatchTouchEvent(event);
+    }
+
+}
