12f2cf65d703df9cd04cc915efc527976503a2b9
e <1130314814@qq.com>
2019-05-25 09:23:26 +0800

    如果有背景色，走onDraw()方法；如果没有背景色，不走onDraw()方法

---
+++ view/src/main/java/com/seniorlibs/view/ui/MyViewGroup.java
@@ -0,0 +1,33 @@
+package com.seniorlibs.view.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ViewGroup;
+
+/**
+ * 如果有背景色，走onDraw()方法；如果没有背景色，不走onDraw()方法
+ */
+public class MyViewGroup extends ViewGroup {
+    private static final String TAG = "MyViewGroup";
+    public MyViewGroup(Context context) {
+        super(context);
+    }
+    public MyViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d(TAG, "onDraw:执行了");
+    }
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        Log.d(TAG, "dispatchDraw:执行了");
+    }
+}
