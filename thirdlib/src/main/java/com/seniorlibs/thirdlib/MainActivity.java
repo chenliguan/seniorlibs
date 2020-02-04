237aafc0d8789aed9b8a10b5e6c50079f6d1bb95
chenliguan <1130314814@qq.com>
2020-02-04 10:16:37 +0800

    [描述]：测试git提交 [原因]： [备注]

--- thirdlib/src/main/java/com/seniorlibs/thirdlib/MainActivity.java
+++ thirdlib/src/main/java/com/seniorlibs/thirdlib/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Button btnRxJava = findViewById(R.id.btn_rx_java);
         btnRxJava.setOnClickListener(this);
     }
