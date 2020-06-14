a5c6d666ecaee97399a73f745dc92248f1d3b2fe
chenliguan <chenliguan@ihuayue.cn>
2020-06-14 19:21:17 +0800

    添加EventBus processor引用

---
+++ thirdlib/src/main/java/com/seniorlibs/thirdlib/MyApplication.java
@@ -0,0 +1,16 @@
+package com.seniorlibs.thirdlib;
+
+import android.app.Application;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class MyApplication extends Application {
+
+    private static final String TAG = "MyApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.builder().addIndex(new MyEventBusIndex()).installDefaultEventBus().register(this);
+    }
+}
