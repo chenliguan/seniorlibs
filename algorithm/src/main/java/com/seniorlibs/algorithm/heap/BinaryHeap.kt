24c6d2f1e6acb0f5a2c96a9ecafcd9d8d986479d
chenliguan <1130314814@qq.com>
2020-09-26 22:21:53 +0800

    添加0. 二叉堆实现模板和40. 最小的k个数

---
+++ algorithm/src/main/java/com/seniorlibs/algorithm/heap/BinaryHeap.kt
@@ -0,0 +1,133 @@
+package com.seniorlibs.algorithm.heap
+
+import java.util.*
+import kotlin.NoSuchElementException
+
+/**
+ * Author: chen
+ * Version: 1.0.0
+ * Date: 2020/9/26
+ * Mender:
+ * Modify:
+ * Description: 二叉堆实现
+ */
+class BinaryHeap(capacity: Int) {
+
+    private val d = 2
+    private var heap: IntArray
+    private var heapSize = 0
+
+    /**
+     * 这将使用默认大小初始化我们的堆。
+     */
+    init {
+        heapSize = 0
+        heap = IntArray(capacity + 1)
+        Arrays.fill(heap, -1)
+    }
+
+    fun isEmpty(): Boolean {
+        return heapSize == 0
+    }
+
+
+    fun isFull(): Boolean {
+        return heapSize == heap.size
+    }
+
+    private fun parent(i: Int): Int {
+        return (i - 1) / d
+    }
+
+    private fun kthChild(i: Int, k: Int): Int {
+        return d * i + k
+    }
+
+    /**
+     * 在堆中插入新元素
+     * 时间复杂性: O(log N)，在最坏的情况下，我们需要遍历到根本
+     */
+    fun insert(x: Int) {
+        if (isFull()) {
+            throw NoSuchElementException("Heap is full, No space to insert new element")
+        }
+        heap[heapSize] = x
+        heapSize++
+        heapifyUp(heapSize - 1)
+    }
+
+    /**
+     * 删除索引为x的元素
+     * Complexity: O(log N)
+     */
+    fun delete(x: Int): Int {
+        if (isEmpty()) {
+            throw NoSuchElementException("Heap is empty, No element to delete")
+        }
+        val maxElement = heap[x]
+        heap[x] = heap[heapSize - 1]
+        heapSize--
+        heapifyDown(x)
+        return maxElement
+    }
+
+
+    /**
+     * 插入元素时保持堆属性。
+     */
+    private fun heapifyUp(i: Int) {
+        var i = i
+        val insertValue = heap[i]
+        while (i > 0 && insertValue > heap[parent(i)]) {
+            heap[i] = heap[parent(i)]
+            i = parent(i)
+        }
+        heap[i] = insertValue
+    }
+
+
+    /**
+     * 在删除元素时保持堆属性。
+     */
+    private fun heapifyDown(i: Int) {
+        var i = i
+        var child: Int
+        val temp = heap[i]
+        while (kthChild(i, 1) < heapSize) {
+            child = maxChild(i)
+            if (temp >= heap[child]) {
+                break
+            }
+            heap[i] = heap[child]
+            i = child
+        }
+        heap[i] = temp
+    }
+
+
+    private fun maxChild(i: Int): Int {
+        val leftChild = kthChild(i, 1)
+        val rightChild = kthChild(i, 2)
+        return if (heap[leftChild] > heap[rightChild]) leftChild else rightChild
+    }
+
+
+    /**
+     * 打印堆的所有元素
+     */
+    fun printHeap() {
+        print("nHeap = ")
+        for (i in 0 until heapSize) print(heap[i].toString() + " ")
+        println()
+    }
+
+
+    /**
+     * 此方法返回堆的max元素。
+     * complexity: O(1)
+     */
+    fun findMax(): Int {
+        if (isEmpty()) throw NoSuchElementException("Heap is empty.")
+        return heap[0]
+    }
+}
--- algorithm/src/main/java/com/seniorlibs/algorithm/heap/HeapActivity.kt
+++ algorithm/src/main/java/com/seniorlibs/algorithm/heap/HeapActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.seniorlibs.algorithm.R
 import com.seniorlibs.baselib.utils.LogUtils
 import java.util.*
+import kotlin.Comparator
 import kotlin.collections.ArrayList
 
 /**
@@ -40,7 +41,9 @@ class HeapActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initView() {
+        findViewById<View>(R.id.btn_heap).setOnClickListener(this)
         findViewById<View>(R.id.btn_top_k_frequent).setOnClickListener(this)
+        findViewById<View>(R.id.btn_get_least_numbers).setOnClickListener(this)
     }
 
     private fun initData() {
@@ -50,11 +53,28 @@ class HeapActivity : AppCompatActivity(), View.OnClickListener {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onClick(v: View) {
         when (v.id) {
+            R.id.btn_heap -> {
+                val maxHeap = BinaryHeap(10)
+                maxHeap.insert(10)
+                maxHeap.insert(4)
+                maxHeap.insert(9)
+                maxHeap.insert(1)
+                maxHeap.insert(7)
+                maxHeap.insert(5)
+                maxHeap.insert(3)
+                maxHeap.printHeap()
+                maxHeap.delete(5)
+                maxHeap.printHeap()
+                maxHeap.delete(2)
+                maxHeap.printHeap()
+            }
             R.id.btn_top_k_frequent -> {
                 val nums: IntArray = intArrayOf(0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6)
-                LogUtils.e(TAG, "347. 前 K 个高频元素 ：" + topKFrequent(nums, 3))
+                LogUtils.d(TAG, "347. 前 K 个高频元素 ：" + topKFrequent(nums, 3))
             }
-
+            R.id.btn_top_k_frequent -> {
+                LogUtils.d(TAG, "40. 最小的k个数 ：" + getLeastNumbers(intArrayOf(3, 2, 1), 2))
+            }
             else -> {
             }
         }
@@ -63,6 +83,9 @@ class HeapActivity : AppCompatActivity(), View.OnClickListener {
     /**
      * 347. 前 K 个高频元素  nums = [0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6] , k = 2
      *
+     * 思路：如果堆的元素个数小于k，就可以直接插入堆中。
+     *      如果堆的元素个数等于k，则检查堆顶与新元素的次数的大小。如果堆顶更大，说明至少有k个数字的出现次数比当前值大，故舍弃当前值；否则，就弹出堆顶，并将当前值插入堆中。
+     *
      * 时间复杂度：O(nlogk)。维护一个大小为k的小顶堆，每次插入元素的时候，需要的时间是logk；
      *          而整个过程中处理n个元素，最坏的情况就是，每次如果新元素的次数比堆顶端的元素大，
      *          则弹出堆顶端的元素，将新的元素添加进堆中，这个过程总共需要做n次，结果是nlogk
@@ -70,6 +93,7 @@ class HeapActivity : AppCompatActivity(), View.OnClickListener {
      * 空间复杂度：O(n)，最坏情况下（每个元素都不同），map需要存储n个键值对，优先队列需要存储k个元素，
      *          因此空间复杂度是O(n)。
      *
+     * https://leetcode-cn.com/problems/top-k-frequent-elements/solution/347-qian-k-ge-gao-pin-yuan-su-by-chen-li-guan/
      * @param nums
      * @param k
      * @return
@@ -85,24 +109,50 @@ class HeapActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
 
-        val queue : PriorityQueue<Int> = PriorityQueue<Int>(object : Comparator<Int> {
-            override fun compare(o1: Int?, o2: Int?): Int {
-                return map[o1]!! - map[o2]!! // 2.1 优先堆队列：按升序用小顶堆保存次数最大的k个元素
-            }
-        })
+        // 2.1 优先堆队列：按升序用小顶堆保存次数最大的k个元素
+        val heap = PriorityQueue(Comparator<Int> { o1, o2 -> map[o1]!! - map[o2]!! })
         for (key in map.keys) {
-            if (queue.size < k) {  // 2.2 如果堆的元素个数小于k，就可以直接插入堆中
-                queue.offer(key)
-            } else if (map[key]!! > map[queue.peek()]!!) {  // 2.3 如果新元素的次数比堆顶端的元素大，则弹出堆顶端的元素，将新的元素添加进堆中
-                queue.poll()
-                queue.offer(key)
+            if (heap.size < k) {   // 2.2 如果堆的元素个数小于k，就可以直接插入堆中
+                heap.offer(key)
+            } else if (map[key]!! > map[heap.peek()]!!) { // 如果堆的元素个数等于k，
+                heap.poll()                               // 2.3 如果新元素的次数比堆顶端的元素大，则弹出堆顶端的元素，将新的元素添加进堆中
+                heap.offer(key)
             }
         }
 
         val list: MutableList<Int> = mutableListOf()   // 3. 最小堆中的k个元素即为前k个高频元素，取出堆中的元素
-        queue.forEach {
-            list.add(it)
+        for (i in heap) {
+            list.add(i)
         }
         return list
+    }
+
+
+    /**
+     * 40. 最小的k个数 - 方法一：堆
+     *
+     * 时间复杂度：O(nlogk)。维护一个大小为k的小顶堆，每次插入元素的时候，需要的时间是logk；
+     *          而整个过程中处理n个元素，最坏的情况就是，每次如果新元素的次数比堆顶端的元素大，
+     *          则弹出堆顶端的元素，将新的元素添加进堆中，这个过程总共需要做n次，结果是nlogk
+     *
+     * 空间复杂度：O(n)，因为小根堆里最多n个数，n是数组的长度。
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    fun getLeastNumbers(arr: IntArray, k: Int): IntArray {
+        // val heap = PriorityQueue<Int>()   // 默认是小根堆
+        val heap = PriorityQueue(Comparator<Int> { o1, o2 -> o1 - o2 })
+        for (c in arr) {    // 将所有数加入到小根堆中
+            heap.offer(c)
+        }
+
+        val res = IntArray(k)   // 从小根堆中取出最小数，取k次
+        for (i in 0 until k) {
+            res[i] = heap.poll()
+        }
+
+        return res
     }
 }
