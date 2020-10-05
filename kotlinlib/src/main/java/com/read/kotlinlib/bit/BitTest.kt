46f11c2bd3c86904a0ea7a42f96e8e5946a01fc4
chenliguan <chenliguan@ihuayue.cn>
2020-10-05 11:06:44 +0800

    修改存储字符注释

--- kotlinlib/src/main/java/com/read/kotlinlib/bit/BitTest.kt
+++ kotlinlib/src/main/java/com/read/kotlinlib/bit/BitTest.kt
@@ -20,26 +20,57 @@ class BitTest {
      * 如何存储字符
      */
     fun storeChar() {
-        val bytesUtf81 = "斗".toByteArray(charset("utf-8"))
-        LogUtils.d(TAG, "bytesUtf81.size：${bytesUtf81.size}")
-        for (b in bytesUtf81) {
-            LogUtils.d(TAG, "bytesUtf81.toHexString：${Integer.toHexString(b.toInt())}")
-        }
-
-        val bytesUtf8 = "斗".toByteArray(charset("utf-16"))
+        val bytesUtf8 = "斗".toByteArray(charset("utf-8"))
         LogUtils.d(TAG, "bytesUtf8.size：${bytesUtf8.size}")
         for (b in bytesUtf8) {
             LogUtils.d(TAG, "bytesUtf8.toHexString：${Integer.toHexString(b.toInt())}")
         }
+//        bytesUtf8.size：3
+//        bytesUtf8.toHexString：ffffffe6
+//        bytesUtf8.toHexString：ffffff96
+//        bytesUtf8.toHexString：ffffff97
 
-        val bytes = "中".toByteArray(charset("utf-16"))
-        LogUtils.d(TAG, "bytes.size：${bytes.size}")
-        for (b in bytes) {
-            LogUtils.d(TAG, "bytes.toHexString：${Integer.toHexString(b.toInt())}")
+        val bytesUtf16 = "斗".toByteArray(charset("utf-16"))
+        LogUtils.d(TAG, "bytesUtf16.size：${bytesUtf16.size}")
+        for (b in bytesUtf16) {
+            LogUtils.d(TAG, "bytesUtf16.toHexString：${Integer.toHexString(b.toInt())}")
+        }
+//        bytesUtf16.size：4
+//        bytesUtf16.toHexString：ffffffff
+//        bytesUtf16.toHexString：fffffffe
+//        bytesUtf16.toHexString：ffffff97
+//        bytesUtf16.toHexString：65
+
+        val bytesUtf = "中".toByteArray(charset("utf-16"))
+        LogUtils.d(TAG, "bytesUtf.size：${bytesUtf.size}")
+        for (b in bytesUtf) {
+            LogUtils.d(TAG, "bytesUtf.toHexString：${Integer.toHexString(b.toInt())}")
+        }
+//        bytesUtf.size：4
+//        bytesUtf.toHexString：ffffffff
+//        bytesUtf.toHexString：fffffffe
+//        bytesUtf.toHexString：2d
+//        bytesUtf.toHexString：4e
+
+        val bytesChar = charArrayOf('A')
+        LogUtils.d(TAG, "bytesChar.size：${bytesChar.size}")
+        for (b in bytesChar) {
+            LogUtils.d(TAG, "bytesChar.toHexString：${Integer.toHexString(b.toInt())}")
         }
 
         // emoji 代表表情
         val byte = Character.toChars(0x1F601)    // 1个笑脸，真实长度是1
         LogUtils.d(TAG, "byte.size：${byte.size} + emoji：${String(byte).length}")  // byte.size：2 + emoji：2
+
+        // 类型转换
+        var n = 'b'
+        LogUtils.d(TAG, "类型转换 n++：${n++}")
+        LogUtils.d(TAG, "类型转换 ++n：${++n}")
+        LogUtils.d(TAG, "类型转换 n+1：${n+1}")
+        LogUtils.d(TAG, "类型转换 n.toInt() + 1：${n.toInt() + 1}")
+//        类型转换 n++：b
+//        类型转换 ++n：d
+//        类型转换 n+1：e
+//        类型转换 n.toInt() + 1：101
     }
 }
