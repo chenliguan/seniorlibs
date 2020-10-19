6cd3bfaa482798593cb6b29894bebf9db3ed053c
chenliguan <1130314814@qq.com>
2020-10-19 21:32:14 +0800

    测试字符串长度，分在栈和堆2中形式

--- kotlinlib/src/main/java/com/read/kotlinlib/bit/ListViewEx2.java
+++ kotlinlib/src/main/java/com/read/kotlinlib/bit/ListViewEx2.java
@@ -5,5 +5,9 @@ public class ListViewEx2 {
         String s1 = "Ja" + "va";
         String s2 = "Java";
         System.out.println(s1 == s2);
+
+        String s3 = new String("Android");
+        byte[] bytes = null;  // 读取文件才是最真实模拟在堆的场景
+        String s4 = new String(bytes);
     }
 }
