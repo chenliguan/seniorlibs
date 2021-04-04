6114c93d67417b89c8479658f5293f4ec55673ba
chenliguan <1130314814@qq.com>
2021-04-04 15:38:42 +0800

    实现 LRUCache

--- algorithm/src/main/java/com/seniorlibs/algorithm/other/LRUCache.kt
+++ algorithm/src/main/java/com/seniorlibs/algorithm/other/LRUCache.kt
@@ -1,144 +1,144 @@
 package com.seniorlibs.algorithm.other
 
 /**
- * 哈希表 + 双向链表：一个哈希表和一个双向链表维护所有在缓存中的键值对
+ * 散列表 + 双向链表
  *
- * 双向链表按照被使用的顺序存储了这些键值对，靠近头部的键值对是最近使用的，而靠近尾部的键值对是最久未使用的。
- * 哈希表即为普通的哈希映射（HashMap），通过缓存数据的键映射到其在双向链表中的位置。
+ * 使用双向链表存储数据，链表中的每个结点处理存储数据（data）、前驱指针（prev）、后继指针（next），
+ * 和散列表通过链表法解决散列冲突，因此增加一个特殊的字段 hnext。
  *
- * 思路：
- * 1.用字典来存储 key-value 结构，这样对于查找操作时间复杂度就是 O(1)；
- * 2.但是因为字典本身是无序的，所以我们还需要一个类似于队列的结构来记录访问的先后顺序。需要支持如下几种操作：在末尾加入一项、去除最前端一项、将队列中某一项移到末尾；
- * 3.首先考虑列表结构，但是列表无法在常数时间内把某个value挑出来移到队尾；
- * 4.再考虑单链表，可以在常数的时间内找到对应的节点，但是如果想将它移到尾部则需要从头遍历到该节点才能保证链表不断，时间复杂度也是O(n)；
- * 5.双链表，解决了移到末尾这个问题；
+ * 所以每个结点会在两条链中。一个链是刚刚我们提到的双向链表，另一个链是散列表中的拉链。
+ * 前驱和后继指针是为了将结点串在双向链表中，hnext 指针是为了将结点串在散列表的拉链中。
  *
+ * 为什么使用双向链表？在删除一个元素时，虽然能 O(1) 的找到目标结点，但是要删除该结点需要拿到前一个结点的指针，遍历到前一个结点复杂度会变为 O(N），所以用双链表实现比较合适。
+ * 为什么要在链表中同时存储 key 和 val，而不是只存储 val？当缓存容量已满，我们不仅仅要删除最后一个 Node 节点，还要把 map 中映射到该节点的 key 同时删除，而这个 key 只能由 Node 得到。
  * @constructor Create empty L r u cache
  */
-class LRUCache(capacity: Int) {
+class LRUCache(private val cap: Int) {
 
-    internal class LinkedNode(var key: Int, var `val`: Int) {
-        var pre: LinkedNode? = null
-        var next: LinkedNode? = null
-    }
+    // key -> Node(key, val)
+    private val map: HashMap<Int, Node?>
 
-    /**
-     * 总容量
-     */
-    private var capacity: Int
-
-    /**
-     * 当前容量
-     */
-    private var size: Int = 0
-
-    /**
-     * 使用伪头部和伪尾部节点
-     */
-    private var head = LinkedNode(0, 0)
-    private var tail = LinkedNode(0, 0)
-
-    /**
-     * 哈希表快速定位节点
-     */
-    private val cache: MutableMap<Int, LinkedNode> = mutableMapOf()
+    // Node(k1, v1) <-> Node(k2, v2)...
+    private val cache: DoubleList
 
     init {
-        this.capacity = capacity
-        head.next = tail
-        tail.pre = head
+        map = HashMap()
+        cache = DoubleList()
     }
 
-    /**
-     * operator允许cache[1]获取
-     *
-     * @param key
-     * @return
-     */
+    /* 将某个 key 提升为最近使用的 */
+    private fun makeRecently(key: Int) {
+        val x = map[key]
+        // 先从链表中删除这个节点
+        cache.remove(x)
+        // 重新插到队尾
+        cache.addLast(x)
+    }
+
+    /* 添加最近使用的元素 */
+    private fun addRecently(key: Int, `val`: Int) {
+        val x = Node(key, `val`)
+        // 链表尾部就是最近使用的元素
+        cache.addLast(x)
+        // 别忘了在 map 中添加 key 的映射
+        map[key] = x
+    }
+
+    /* 删除某一个 key */
+    private fun deleteKey(key: Int) {
+        val x = map[key]
+        // 从链表中删除
+        cache.remove(x)
+        // 从 map 中删除
+        map.remove(key)
+    }
+
+    /* 删除最久未使用的元素 */
+    private fun removeLeastRecently() {
+        // 链表头部的第一个元素就是最久未使用的
+        val deletedNode = cache.removeFirst()
+        // 同时别忘了从 map 中删除它的 key
+        val deletedKey = deletedNode!!.key
+        map.remove(deletedKey)
+    }
+
     operator fun get(key: Int): Int {
-        val node = cache[key] ?: return -1
-        // 如果 key 存在，先通过哈希表定位，再移到头部
-        moveToHead(node)
-        return node.`val`
-    }
-
-    /**
-     * 添加key-value
-     *
-     * @param key
-     * @param val
-     */
-    fun put(key: Int, `val`: Int) {
-        val node = cache[key]
-        if (node == null) {
-            // 如果 key 不存在，创建一个新的节点
-            val newNode = LinkedNode(key, `val`)
-            // 添加进哈希表
-            cache[key] = newNode
-            // 添加至双向链表的头部
-            addToHead(newNode)
-
-            if (size > capacity) {
-                // 如果超出容量，删除双向链表的尾部节点
-                val tail = removeTail()
-                // 删除哈希表中对应的项
-                cache.remove(tail.key)
-            }
-        } else {
-            // 如果 key 存在，先通过哈希表定位，再修改 `val`，并移到头部
-            node.`val` = `val`
-            moveToHead(node)
+        if (!map.containsKey(key)) {
+            return -1
         }
+        // 将该数据提升为最近使用的
+        makeRecently(key)
+        return map[key]!!.`val`
     }
 
-    /**
-     * 添加至双向链表的头部
-     *
-     * @param node
-     */
-    private fun addToHead(node: LinkedNode) {
-        // 处理右指向
-        node.next = head.next
-        head.next?.pre = node
-
-        // 处理左指向
-        node.pre = head
-        head.next = node
-
-        size++
-    }
-
-    /**
-     * 移动到双向链表的头部
-     *
-     * @param node
-     */
-    private fun moveToHead(node: LinkedNode) {
-        removeNode(node)
-        addToHead(node)
-    }
-
-    /**
-     * 删除双向链表的尾部节点
-     *
-     * @return
-     */
-    private fun removeTail(): LinkedNode {
-        val node: LinkedNode = tail.pre!!
-        removeNode(node)
-        return node
+    fun put(key: Int, `val`: Int) {
+        if (map.containsKey(key)) {
+            // 删除旧的数据
+            deleteKey(key)
+            // 新插入的数据为最近使用的数据
+            addRecently(key, `val`)
+            return
+        }
+        if (cap == cache.size()) {
+            // 删除最久未使用的元素
+            removeLeastRecently()
+        }
+        // 添加为最近使用的元素
+        addRecently(key, `val`)
     }
 
 
-    /**
-     * 删除双向链表的当前节点
-     *
-     * @param node
-     */
-    private fun removeNode(node: LinkedNode) {
-        node.pre?.next = node.next
-        node.next?.pre = node.pre
 
-        size--
+    internal class Node(var key: Int, var `val`: Int) {
+        var next: Node? = null
+        var prev: Node? = null
+    }
+
+    internal class DoubleList {
+        // 头尾虚节点
+        private val head: Node
+        private val tail: Node
+
+        // 链表元素数
+        private var size: Int
+
+        // 在链表尾部添加节点 x，时间 O(1)
+        fun addLast(x: Node?) {
+            x!!.prev = tail.prev
+            x.next = tail
+            tail.prev!!.next = x
+            tail.prev = x
+            size++
+        }
+
+        // 删除链表中的 x 节点（x 一定存在）
+        // 由于是双链表且给的是目标 Node 节点，时间 O(1)
+        fun remove(x: Node?) {
+            x!!.prev!!.next = x.next
+            x.next!!.prev = x.prev
+            size--
+        }
+
+        // 删除链表中第一个节点，并返回该节点，时间 O(1)
+        fun removeFirst(): Node? {
+            if (head.next === tail) return null
+            val first = head.next
+            remove(first)
+            return first
+        }
+
+        // 返回链表长度，时间 O(1)
+        fun size(): Int {
+            return size
+        }
+
+        init {
+            // 初始化双向链表的数据
+            head = Node(0, 0)
+            tail = Node(0, 0)
+            head.next = tail
+            tail.prev = head
+            size = 0
+        }
     }
 }
