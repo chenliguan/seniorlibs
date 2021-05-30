1253314552ed4d5074dc922e2748650179f531d8
chenliguan <1130314814@qq.com>
2021-05-30 22:57:29 +0800

    406. 根据身高重建队列

--- algorithm/src/main/java/com/seniorlibs/algorithm/array/ArrayActivity.kt
+++ algorithm/src/main/java/com/seniorlibs/algorithm/array/ArrayActivity.kt
@@ -64,6 +64,7 @@ class ArrayActivity : AppCompatActivity(), View.OnClickListener {
         findViewById<View>(R.id.btn_largest_number).setOnClickListener(this)
         findViewById<View>(R.id.btn_count_and_say).setOnClickListener(this)
         findViewById<View>(R.id.btn_longest_consecutive).setOnClickListener(this)
+        findViewById<View>(R.id.btn_reconstruct_queue).setOnClickListener(this)
         findViewById<View>(R.id.btn_can_finish).setOnClickListener(this)
     }
 
@@ -219,6 +220,18 @@ class ArrayActivity : AppCompatActivity(), View.OnClickListener {
         }
         R.id.btn_longest_consecutive -> {
             LogUtils.d(TAG, "128. 最长连续序列：${longestConsecutive(intArrayOf(100, 4, 200, 1, 3, 2))}")
+        }
+        R.id.btn_reconstruct_queue -> {
+            // [7,0],[4,4],[7,1],[5,0],[6,1],[5,2]
+            val nums = arrayOf(
+                    intArrayOf(7, 0),
+                    intArrayOf(4, 4),
+                    intArrayOf(7, 1),
+                    intArrayOf(5, 0),
+                    intArrayOf(6, 1),
+                    intArrayOf(5, 2)
+            )
+            LogUtils.d(TAG, "406. 根据身高重建队列：${reconstructQueue(nums)}")
         }
         R.id.btn_can_finish -> {
             val array = arrayOf(
@@ -1598,6 +1611,51 @@ class ArrayActivity : AppCompatActivity(), View.OnClickListener {
             res = Math.max(res, r - num + 1)
         }
         return res
+    }
+
+
+    /**
+     * 406. 根据身高重建队列
+     * 思想：高个子先站好位，矮个子插入到K位置上，前面肯定有K个高个子，矮个子再插到前面，也满足K的要求
+     *
+     * [7,0], [4,4], [7,1], [5,0], [6,1], [5,2]  --> 排序前
+     * [7,0], [7,1], [6,1], [5,0], [5,2], [4,4]  --> 排序后
+     * 再一个一个插入。
+     * [7,0]                                      // 将 [7, 0] 插入到 i = 0 处
+     * [7,0], [7,1]                               // 将 [7, 1] 插入到 i = 1 处
+     * [7,0], [6,1], [7,1]                        // 将 [6, 1] 插入到 i = 1 处，如果原来位置有数据了，会往后移（关键）
+     * [5,0], [7,0], [6,1], [7,1]                 // 将 [5, 0] 插入到 i = 0 处
+     * [5,0], [7,0], [5,2], [6,1], [7,1]          // 将 [5, 2] 插入到 i = 2 处
+     * [5,0], [7,0], [5,2], [6,1], [4,4], [7,1]   // 将 [4, 4] 插入到 i = 4 处
+     *
+     * 时间复杂度：O(n^2)，其中 n 是数组 people 的长度。我们需要 O(nlogn) 的时间进行排序，随后需要 O(n^2) 的时间遍历每一个人并将他们放入队列中。
+     * 空间复杂度：O(logn)，即为排序需要使用的栈空间。
+     *
+     * https://leetcode-cn.com/problems/queue-reconstruction-by-height/solution/406-gen-ju-shen-gao-zhong-jian-dui-lie-b-99hr/
+     *
+     * @param array
+     * @return
+     */
+    fun reconstructQueue(array: Array<IntArray>): Array<IntArray>? {
+        val res = LinkedList<IntArray>()
+
+        // 1.排序规则：先按身高降序，若身高相同则按第二个数字升序
+        Arrays.sort(array) { o1, o2 ->
+            if (o1[0] != o2[0]) {
+                // 按身高降序
+                o2[0] - o1[0]
+            } else {
+                // 按第二个数字升序
+                o1[1] - o2[1]
+            }
+        }
+
+        // 2.遍历排序后的数组，第二个数字作为索引位置，把数组插入到目标索引位置上，如果原来位置有数据了，会往后移。
+        for (i in array) {
+            res.add(i[1], i)
+        }
+
+        return res.toArray(Array(res.size) { IntArray(2) })
     }
 
 
